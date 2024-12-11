c98478556f4b1029369950a93d9940ab2fa081f8
citraaa12 <112358741+citraaa12@users.noreply.github.com>
2024-12-11 23:50:01 +0700

    Update apk.py

--- apk.py
+++ apk.py
@@ -359,7 +359,6 @@ with st.container():
         model.fit(X_train, y_train, epochs=5, batch_size=32, validation_split=0.1, verbose=1)
         
         # Streamlit App
-        st.title(" ")
         input_text = st.text_area("Masukkan komentar YouTube :")
         
         if st.button("Prediksi"):
